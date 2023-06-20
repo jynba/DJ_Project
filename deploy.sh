4d15a14a4fa8f448dd390483ff6d45009ba242b4
jack <925812362@qq.com>
2023-06-20 16:19:24 +0800

    fix:添加命名功能

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env sh
+###
+ # @Descripttion: 
+ # @version: 1.0.0
+ # @Author: 朱海东
+ # @Date: 2023-06-20 15:38:35
+ # @LastEditTime: 2023-06-20 16:16:26
+### 
 
 # 确保脚本抛出遇到的错误
 set -e
@@ -11,6 +18,6 @@ git add -A
 git commit -m 'fix:添加命名功能'
 
 # 直接部署到github,触发github action
-git push git@github.com:jynba/DJ_Project.git main
+git push 
 
 cd -
